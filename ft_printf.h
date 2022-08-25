61d63f6d0811d23cef5763f494cfc28adb8935c0
taboterm <taboterm@student.42wolfsburg.de>
2022-08-25 15:06:41 +0200

    printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:24:52 by taboterm          #+#    #+#             */
-/*   Updated: 2022/08/24 19:15:15 by taboterm         ###   ########.fr       */
+/*   Updated: 2022/08/25 14:47:14 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-
 int			ft_printf(const char *fmt, ...);
 int			ft_printchar(int c);
 int			ft_len(long int n);
@@ -25,5 +24,6 @@ int			ft_uitoa(unsigned long int n);
 int			ft_putnum_mod(long n, char *base, int base_len);
 int			ft_printptr(unsigned long long ptr);
 int			ft_pt_len(unsigned long int addi);
+int			ft_printstr(char *str);
 
 #endif
